ce8802d7a22a0e06ec6fa84b9277248ade875d10
malne <malnen13@gmail.com>
2025-03-10 12:17:54 +0100

    Bluno clock

---
+++ Bluno_Clock.ino
@@ -0,0 +1,58 @@
+int rPin = 11;
+int gPin = 10;
+int bPin = 9;
+
+unsigned long lastActivityTime = 0;
+
+int pulseValue = 0;
+int pulseDirection = 1;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(rPin, OUTPUT);
+  pinMode(gPin, OUTPUT);
+  pinMode(bPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+    processRGBData(data);
+    lastActivityTime = millis();
+  }
+
+  if (millis() - lastActivityTime >= 10000) {
+    slowPulse();
+    delay(25);
+  }
+}
+
+void processRGBData(String data) {
+  data.trim();
+  if (data.startsWith("[") && data.endsWith("]")) {
+    data = data.substring(1, data.length() - 1);
+    int firstComma = data.indexOf(',');
+    int secondComma = data.lastIndexOf(',');
+
+    int r = data.substring(0, firstComma).toInt();
+    int g = data.substring(firstComma + 1, secondComma).toInt();
+    int b = data.substring(secondComma + 1).toInt();
+
+    analogWrite(rPin, 255 - r);
+    analogWrite(gPin, 255 - g);
+    analogWrite(bPin, 255 - b);
+
+    lastActivityTime = millis();
+  }
+}
+
+void slowPulse() {
+  pulseValue += pulseDirection * 5;
+  if (pulseValue <= 0 || pulseValue >= 255) {
+    pulseDirection = -pulseDirection;
+  }
+
+  analogWrite(rPin, pulseValue);
+  analogWrite(gPin, pulseValue);
+  analogWrite(bPin, pulseValue);
+}
